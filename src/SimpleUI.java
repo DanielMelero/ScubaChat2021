56bbb605f845d7c02f545eaf1801d5aff1f50388
KeVr000 <k.verlaan-1@student.utwente.nl>
2021-04-07 09:23:20 +0200

    implemented keyboard input for UI as a separate Thread
    tests are to come next

--- src/SimpleUI.java
+++ src/SimpleUI.java
@@ -1,5 +1,7 @@
 package src;
 
+import java.util.Scanner;
+
 /**
  * a simple CLI for testing
  * 
@@ -12,11 +14,30 @@ package src;
 public class SimpleUI {
 	
 	// this class handles the input from the user as a separate thread
-	class ImputHandler extends Thread{
+	class InputHandler extends Thread{
+		Scanner sc;
+		ApplicationLayer layer;
+		
+		public InputHandler(ApplicationLayer layer) {
+			sc = new Scanner(System.in);
+			this.layer = layer;
+		}
 		
 		public void run() {
-			
+			while(true) {
+				System.out.print("> ");
+				String input = sc.nextLine();
+				layer.sendMessage(input);
+			}
 		}
+	}
+	
+	public void showMessage(String msg) {
+		System.out.println(msg);
+	}
+	
+	public static void main(String[] args) {
+		
 	}
 
 }
