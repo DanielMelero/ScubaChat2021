6d7af529c12d01baed7421735ace8297f659e015
KeVr000 <k.verlaan-1@student.utwente.nl>
2021-04-07 09:04:10 +0200

    added classes for UI and application layer
    both are still non functioning

---
+++ src/ApplicationLayer.java
@@ -0,0 +1,30 @@
+package src;
+
+/**
+ * Application Layer mechanism
+ * 
+ * @author Kento Verlaan
+ * 
+ * methods used to send and receive messages from and to others
+ *
+ */
+
+public class ApplicationLayer {
+	
+	/**
+	 * method to get the message from others
+	 * @return the message as a string
+	 */
+	public String getMessage() {
+		return null;
+	}
+	
+	/**
+	 * method to send the message, invokes a function in the lower layer
+	 * @param msg the message that should be sent as a String
+	 */
+	public void sendMessage(String msg) {
+		
+	}
+
+}
---
+++ src/SimpleUI.java
@@ -0,0 +1,22 @@
+package src;
+
+/**
+ * a simple CLI for testing
+ * 
+ * @author Kento Verlaan
+ * 
+ * this CLI is very basic and should be improved on a later stage
+ *
+ */
+
+public class SimpleUI {
+	
+	// this class handles the input from the user as a separate thread
+	class ImputHandler extends Thread{
+		
+		public void run() {
+			
+		}
+	}
+
+}
